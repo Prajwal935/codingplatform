30ca07d0a0dd704752f6c107b6e408b4b4d35dd9
Prajwal <prajwalgowda93535@gmail.com>
2024-11-09 23:55:08 +0530

    NavBar done

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Navbar(){
+  return(
+    <header className="header">
+
+        <div className="left-section">
+            <img className="code-logo" src="./logo192.png" alt="Code Platform Logo"/>
+        </div>
+
+        <div className="middle-section">
+            <img className="dark-mode" src="./images/choose-dark-mode.png" alt="Dark Mode Icon"/>
+            <ul className="nav-items">
+                <li>Home</li>
+                <li>Contact</li>
+                <li>Practice</li>
+                <li>Contest</li>
+            </ul>
+        </div>
+
+        <div className="right-section">
+            <img className="nav-profile" src="./images/nav-profile.png" alt="Profile Icon"/>
+        </div>
+    </header>
+  );
+}
---
+++ src/SelectionPage.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function Selection(){
+    return(
+        <div className="SelectionPage">
+            <div className="choose-topics">
+                Topics
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>sdf</li>
+                    <li>sdf</li>
+                    <li>sdf</li>
+                </ul>
+            </div>
+            <div className="random-problems">
+                Random Problems
+            </div>
+        </div>
+    );
+}
